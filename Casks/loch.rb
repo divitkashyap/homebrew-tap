f487fdcf0f6cc6d3d41d0fae4f2a2d9c7263f409
Divit Kashyap <162712154+divitkashyap@users.noreply.github.com>
2026-07-10 22:44:57 +0100

    cask: bump to v1.2.1

--- Casks/loch.rb
+++ Casks/loch.rb
@@ -1,8 +1,8 @@
 cask "loch" do
-  version "1.2.0"
-  sha256 "be7d6700b3a6b1b28c1d84cecbfe821980149228891fd15e083eaa4d5d434252"
+  version "1.2.1"
+  sha256 "1089ace621f62390e01664b155c6a4e62ce4de07efe0e9c6f923da02c0e3d74d"
 
-  url "https://github.com/divitkashyap/loch-app/releases/download/v#{version}/Loch-v#{version}-2-unsigned.zip"
+  url "https://github.com/divitkashyap/loch-app/releases/download/v#{version}/Loch-v#{version}-3-unsigned.zip"
   name "Loch"
   desc "Developer's dynamic island for the MacBook notch"
   homepage "https://github.com/divitkashyap/loch-app"
